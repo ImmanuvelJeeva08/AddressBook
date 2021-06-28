8d0f876ff42c2b26ce2b46a70ef5794abe6c749c
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-06-28 09:55:27 -0700

    Use case 4 added

---
+++ Uc4.java
@@ -0,0 +1,19 @@
+/**
+ * To delete a person using their name
+ */
+
+import java.util.Scanner;
+
+public class Uc4 {
+
+    // get user name and remove contact of that user
+
+    public static void removeContacts(){
+        Scanner sc4 = new Scanner(System.in);
+
+        System.out.println("Enter name of which person contact you delete");
+        String delete = sc4.next();
+
+        ContactsMain.arr.removeIf(i -> delete.equals(i.getFirstName()));
+    }
+}
